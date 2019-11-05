e5d53156c0577c9c3823f24d0f629f398f381d6c
walosha <walosha@yahoo.com>
2019-11-05 09:25:15 +0100

    finished implementing the tab component

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ document
   .addEventListener("click", function() {
     document.querySelector(".menu__backdrop").classList.toggle("remove-nav");
   });
+
+// TOGGLE TAB MENU
+
+const tabPanels = [...document.querySelectorAll(".tab__panel")];
+
+tabPanels.forEach(panel =>
+  panel.addEventListener("click", function() {
+    if (this.dataset.tab) {
+      const tabContents = [...document.querySelectorAll(".tab__content-panel")];
+      tabContents.forEach(tabContent => {
+        tabContent.classList.remove("display-tab");
+        this.classList.remove("red-border");
+        tabContent.classList.remove("tab__content-panel-1");
+        if (tabContent.id === this.dataset.tab) {
+          tabContent.classList.add("display-tab");
+        }
+      });
+    }
+  })
+);
